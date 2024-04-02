b3d8004a231f2df92ab4dd420c33a413fdeef23a
chaiaq <chaiane.ey@gmail.com>
2024-04-02 20:31:18 -0300

    02/04

--- oo-sabor-express/modelos/restaurante.py
+++ oo-sabor-express/modelos/restaurante.py
@@ -1,15 +1,23 @@
 
 class Restaurante:
+    restaurantes = []
     # a função init é o método construtor
     # usamos self pra mostrar que se trata deste objeto
+    # semelhante ao 'this' do java
     def __init__(self, nome, categoria):
         self.nome = nome
         self.categoria = categoria
         self.ativo = False
+        Restaurante.restaurantes.append(self)
 
     # str mostra o objeto em formato de texto ao invés do endereço de memória
     def __str__(self):
         return f'{self.nome} | {self.categoria}'
+    
+    # método para listar os restaurantes
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
 
 restaurante_praca = Restaurante('Praça', 'Italiana')
 # restaurante_praca.nome = 'Praça'
@@ -28,5 +36,7 @@ print(vars(restaurante_praca))
 print(restaurante_pizza)
 # dir mostra todos os métodos e atributos
 # print(dir(restaurante_pizza))
+
+Restaurante.listar_restaurantes()
 
 
